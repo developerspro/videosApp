9f2a8503a64b381d904078a2b3c0a5f3be1bfac4
Jeferson Ferreira <jeferson.oferreira@etec.sp.gov.br>
2021-06-06 17:03:14 -0300

    criacao da interface IFilme e interface

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilme } from '../models/IFilme.model';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,27 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  titulo = 'Videos App';
+  listaVideos: IFilme[] = [
+    {
+      nome: 'TakeDown',
+      lancamento: '28/09/2004',
+      duracao: '1h 36m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vQk9GLOjM7gAfqHdWWzt7emGP16.jpg',
+      generos:['Thriller', 'Crime', 'Ação', 'Drama']
+
+    },
+    {
+      nome: 'Ameaça Virtual',
+      lancamento: '12/01/2001',
+      duracao: '1h 48m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hWSyHulb0bNThDCRvJEFvWv4J2a.jpg',
+      generos: ['Ação', 'Crime', 'Drama']
+    },
+
+  ];
   constructor(public alertController: AlertController,public toastController: ToastController) {}
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -36,7 +58,7 @@ export class Tab1Page {
     const toast = await this.toastController.create({
       message: 'Filme adicionado aos favoritos!',
       duration: 2000,
-      color: 'success'
+      color: 'kawasaki'
 
     });
     toast.present();
