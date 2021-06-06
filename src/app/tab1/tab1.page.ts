6702d6de5956a7c9691a0e26228b9f33667727bd
Jeferson Ferreira <jeferson.oferreira@etec.sp.gov.br>
2021-06-06 15:59:06 -0300

    inclusao do toast

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,8 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  constructor(public alertController: AlertController,public toastController: ToastController) {}
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message: 'Deseja realmente favoritar o filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim, favoritar',
+          handler: () => {
+            this.apresentarToast();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Filme adicionado aos favoritos!',
+      duration: 2000,
+      color: 'success'
+
+    });
+    toast.present();
+  }
 
 
-  constructor() {}
 
 }
